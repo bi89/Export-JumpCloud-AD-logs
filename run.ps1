fd468a30376aee97a02b00f27ad28cd4281380e3
Suchathit Boonnag <bi1989@gmail.com>
2023-10-17 13:13:47 +0800

    add computer name

--- run.ps1
+++ run.ps1
@@ -4,11 +4,12 @@
 #>
 
 $time = Get-Date -UFormat "%B_%d_%Y_%T" | ForEach-Object { $_ -replace ":", "-" }  2> $null
+$comname = (Get-ComputerInfo | select CsDNSHostName).CsDNSHostName
 
-New-Item -Path "C:\Windows\Temp\JumpCloud_Log_$time" -ItemType Directory
-Copy-Item -Path "C:\Windows\Temp\JumpCloud_AD_Integration.log" -Destination "C:\Windows\Temp\JumpCloud_Log_$time"
-Copy-Item -Path "C:\Program Files\JumpCloud\AD Sync\adsync.log" -Destination "C:\Windows\Temp\JumpCloud_Log_$time"
-Copy-Item -Path "C:\Program Files\JumpCloud AD Bridge\adint.config.json" -Destination "C:\Windows\Temp\JumpCloud_Log_$time"
+New-Item -Path "C:\Windows\Temp\JumpCloud-AD-Log-$comname-$time" -ItemType Directory
+Copy-Item -Path "C:\Windows\Temp\JumpCloud_AD_Integration.log" -Destination "C:\Windows\Temp\JumpCloud-AD-Log-$comname-$time"
+Copy-Item -Path "C:\Program Files\JumpCloud\AD Sync\adsync.log" -Destination "C:\Windows\Temp\JumpCloud-AD-Log-$comname-$time"
+Copy-Item -Path "C:\Program Files\JumpCloud AD Bridge\adint.config.json" -Destination "C:\Windows\Temp\JumpCloud-AD-Log-$comname-$time"
 
-Compress-Archive -Path "C:\Windows\Temp\JumpCloud_Log_$time" -DestinationPath "C:\Windows\Temp\JumpCloud_Log_$time.zip"
-Move-Item -Path "C:\Windows\Temp\JumpCloud_Log_$time.zip" -Destination "$([Environment]::GetFolderPath('Desktop'))\JumpCloud_Log_$time.zip"
+Compress-Archive -Path "C:\Windows\Temp\JumpCloud-AD-Log-$comname-$time" -DestinationPath "C:\Windows\Temp\JumpCloud-AD-Log-$comname-$time.zip"
+Move-Item -Path "C:\Windows\Temp\JumpCloud-AD-Log-$comname-$time.zip" -Destination "$([Environment]::GetFolderPath('Desktop'))\JumpCloud-AD-Log-$comname-$time.zip"
